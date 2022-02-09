2bae8d4c1f3cadd0f7ad0bdf67b326dfe0171921
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2022-02-09 22:25:52 +0000

    New player control for movement
    This is an new control for network movement between all players and makes sure everything is synced up

---
+++ Assets/David/Scripts/Player/PlayerCameraFollow.cs
@@ -0,0 +1,32 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraFollow : Singleton<PlayerCameraFollow>
+{
+    [SerializeField]
+    private float amplitudeGain = 0.5f;
+
+    [SerializeField]
+    private float frequencyGain = 0.5f;
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private void Awake()
+    {
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void FollowPlayer(Transform transform)
+    {
+        // not all scenes have a cinemachine virtual camera so return in that's the case
+        if (cinemachineVirtualCamera == null) return;
+
+        cinemachineVirtualCamera.Follow = transform;
+
+        var perlin = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        perlin.m_AmplitudeGain = amplitudeGain;
+        perlin.m_FrequencyGain = frequencyGain;
+    }
+}
